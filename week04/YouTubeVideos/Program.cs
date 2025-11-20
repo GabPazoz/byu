aa88c122e16c9d016a5fd789ce503bc20fb04a87
GabPazoz <101400653+GabPazoz@users.noreply.github.com>
2025-11-20 10:03:10 -0300

    W04 Assignment: YouTube Video Program

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        // Create videos
+        Video v1 = new Video("Learn C# in 10 Minutes", "Tech Academy", 600);
+        Video v2 = new Video("The Book of Mormon Story", "Church Channel", 480);
+        Video v3 = new Video("BYU-Idaho Campus Tour", "Rexburg Life", 300);
+
+        // Add comments
+        v1.AddComment(new Comment("John", "Great explanation!"));
+        v1.AddComment(new Comment("Maria", "Helped me a lot, thanks!"));
+        v1.AddComment(new Comment("Luke", "Please make more videos!"));
+
+        v2.AddComment(new Comment("Ana", "Beautiful message."));
+        v2.AddComment(new Comment("Peter", "Very spiritual."));
+        v2.AddComment(new Comment("Sarah", "I loved it!"));
+
+        v3.AddComment(new Comment("Jake", "Looks amazing!"));
+        v3.AddComment(new Comment("Emily", "I want to study there!"));
+        v3.AddComment(new Comment("Paul", "Great tour."));
+
+        // Display
+        v1.Display();
+        v2.Display();
+        v3.Display();
     }
 }
---
+++ week04/YouTubeVideos/video.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+    public List<Comment> Comments { get; private set; } = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return Comments.Count;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {Length} seconds");
+        Console.WriteLine($"Comments ({GetCommentCount()}):");
+
+        foreach (var c in Comments)
+        {
+            Console.WriteLine($"  - {c.Name}: {c.Text}");
+        }
+
+        Console.WriteLine();
+    }
+}
